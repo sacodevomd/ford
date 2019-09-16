f95c0fd5393fff956a0de42b5ffbfe885009a5f9
unknown <scardona@omd.com.gt>
2019-09-16 10:19:05 -0600

    version casi final

---
+++ assets/js/eventos.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+    if(isMobile.any()) {
+
+
+        if(window.innerHeight > window.innerWidth){
+             
+            $("#slide .fondo1").attr("src"," ./assets/mobil/sec01.jpg");
+            $("#conexion > div > img").attr("src"," ./assets/mobil/sec06.jpg");
+        }
+
+        $(window).on("orientationchange", function(event) {
+
+            if(window.innerHeight > window.innerWidth){
+                $("#slide .fondo1").attr("src","./assets/images/seccion01/fondo1.jpg");
+                $("#conexion > div > img").attr("src","   ./assets/images/seccion05/foto05.jpg");
+            }else{
+            $("#slide .fondo1").attr("src"," ./assets/mobil/sec01.jpg");
+            $("#conexion > div > img").attr("src"," ./assets/mobil/sec06.jpg");
+            }
+        });
+
+     }
+
+
+});
